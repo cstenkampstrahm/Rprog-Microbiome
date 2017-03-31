5cd1bd5357f620deec445262dbd4acdef0d0993b
Chloe Stenkamp-Strahm <cstenkampstrahm@gmail.com>
2017-03-31 12:00:51 -0600

    made comparisons with potential confounders in alpha div models

--- Alpha_div_modeling.R
+++ Alpha_div_modeling.R
@@ -121,9 +121,9 @@ avg_alpha_div_scaled <- avg_alpha_div_scaled %>% mutate(Path_1.0 = as.factor(Pat
                                           EvNev_1 = as.factor(EvNev_1),
                                           Pathotype_1 = as.factor(Pathotype_1))
 
-m1.1 <- glmer(Pathotype_1 ~ Scaledrich + (1|Individual_animal), data = alpha_div_scaled,
+m1.1.0 <- glmer(Pathotype_1 ~ Scaledrich + (1|Individual_animal), data = alpha_div_scaled,
               family = binomial, control = glmerControl(optimizer = "bobyqa"))
-summary(m1.1)
+summary(m1.1.0)
 
 # not getting any errors now
 
@@ -165,6 +165,26 @@ z <- summary(m3.1)$coefficients/summary(m3.1)$standard.errors
 z
 p <- (1-pnorm(abs(z), 0, 1)) * 2  
 p  
+
+# is it appropriate to calc the p vals with a normal distribution when
+# the avgrich isn't normally distributed? Check normality of avgrich and
+# scaled avgrich, and look at model with scaled avg rich
+
+shapiro.test(avg_alpha_div_scaled$Avgrich)
+shapiro.test(avg_alpha_div_scaled$Avgscaledrich)
+qqnorm(avg_alpha_div_scaled$Avgrich, ylab= "Average Richness")
+qqnorm(avg_alpha_div_scaled$Avgscaledrich, ylab= "Average Richness Scaled")
+
+# both have sig p vals, not normally distributed. quantile plots look 
+# pretty normal though. 
+
+m3.1.0 <- multinom(Pattern_1 ~ Avgscaledrich, data = avg_alpha_div_scaled)
+summary(m3.1.0)
+z <- summary(m3.1.0)$coefficients/summary(m3.1.0)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+
 
 m3.2 <- multinom(Pattern_1 ~ Avgshann, data = avg_alpha_div_scaled)
 summary(m3.2)
@@ -208,3 +228,220 @@ IQR_normeven
 IQR_avgeven <- IQR(avg_alpha_div_scaled$Avgeven)
 IQR_avgeven
 # 0.03348146
+
+# the only model that is significant is one that looks for association between 
+# multiday shedder and richness. Look at spread of avgrich values to make sure 
+# no outliers:
+avgrichspread <- avg_alpha_div_scaled %>% group_by(Pattern_1) %>% summarize(mean(Avgrich), 
+                                                             min(Avgrich), 
+                                                             max(Avgrich))
+avgrichspread
+# # A tibble: 3 × 4
+#       Pattern_1 `mean(Avgrich)` `min(Avgrich)` `max(Avgrich)`
+#       <fctr>        <dbl>          <dbl>          <dbl>
+#1         0        4186.431         3359.8         5180.0
+#2         1        4252.625         2877.4         7644.4
+#3         2        3842.000         2440.0         5000.4
+
+# others
+avgshannspread <- avg_alpha_div_scaled %>% group_by(Pattern_1) %>% summarize(mean(Avgshann), 
+                                                                     min(Avgshann), 
+                                                                     max(Avgshann))
+avgshannspread
+
+avgevenspread <- avg_alpha_div_scaled %>% group_by(Pattern_1) %>% summarize(mean(Avgeven), 
+                                                                     min(Avgeven), 
+                                                                     max(Avgeven))
+avgevenspread
+
+richspread <- alpha_div_scaled %>% group_by(Pattern_1) %>% summarize(mean(Normrich), 
+                                                                 min(Normrich), 
+                                                                 max(Normrich))
+richspread
+
+# Look at adding different variables to models and see how coefficients change
+# Variables of interest are: Disease, Parity, DIM, Farm
+# m1.1 richness and pathotype (cow randome)
+# m1.1.0 scaled richness and pathotype (cow random)
+# m1.2 shannons and pathotype (cow random)
+# m 1.3 evenness and pathotype (cow random)
+
+alpha_div_scaled <- alpha_div_scaled %>% mutate("Parity" = as.factor(Parity),
+                                                "DIM" = as.numeric(DIM),
+                                                "Disease" = as.factor(Disease),
+                                                "Farm" = as.factor(Farm))
+avg_alpha_div_scaled <- avg_alpha_div_scaled %>% mutate("Parity" = as.factor(Parity),
+                                                "DIM" = as.numeric(DIM),
+                                                "Disease" = as.factor(Disease),
+                                                "Farm" = as.factor(Farm))
+# run model again with the right scaled data set
+m1.1 <- glmer(Pathotype_1 ~ Normrich + (1|Individual_animal), data = alpha_div_scaled,
+              family = binomial, control = glmerControl(optimizer = "bobyqa"))
+summary(m1.1)
+
+##PATHOTYPE
+m1.1farm <- update(m1.1, .~. + Farm)
+summary(m1.1farm)
+m1.1Parity <-  update(m1.1, .~. + Parity)
+summary(m1.1Parity)
+m1.1Disease <- update(m1.1, .~. + Disease)
+summary(m1.1Disease)
+m1.1DIM <-  update(m1.1, .~. + DIM)
+summary(m1.1DIM)
+  
+m1.1.0farm <- update(m1.1.0, .~. + Farm)
+summary(m1.1.0farm)
+m1.1.0Parity <-  update(m1.1.0, .~. + Parity)
+summary(m1.1.0Parity)
+m1.1.0Disease <- update(m1.1.0, .~. + Disease)
+summary(m1.1.0Disease)
+m1.1.0DIM <-  update(m1.1.0, .~. + DIM)
+summary(m1.1.0DIM)
+
+m1.2farm <- update(m1.2, .~. + Farm)
+summary(m1.2farm)
+m1.2Parity <-  update(m1.2, .~. + Parity)
+summary(m1.2Parity)
+m1.2Disease <- update(m1.2, .~. + Disease)
+summary(m1.2Disease)
+m1.2DIM <-  update(m1.2, .~. + DIM)
+summary(m1.2DIM)
+
+m1.3farm <- update(m1.3, .~. + Farm)
+summary(m1.3farm)
+m1.3Parity <-  update(m1.3, .~. + Parity)
+summary(m1.3Parity)
+m1.3Disease <- update(m1.3, .~. + Disease)
+summary(m1.3Disease)
+m1.3DIM <-  update(m1.3, .~. + DIM)
+summary(m1.3DIM)
+
+##EVERNEVER
+
+m2.1farm <- update(m2.1, .~. + Farm)
+summary(m2.1farm)
+m2.1Parity <-  update(m2.1, .~. + Parity)
+summary(m2.1Parity)
+m2.1Disease <- update(m2.1, .~. + Disease)
+summary(m2.1Disease)
+m2.1DIM <-  update(m2.1, .~. + DIM)
+summary(m2.1DIM)
+
+m2.2farm <- update(m2.2, .~. + Farm)
+summary(m2.2farm)
+m2.2Parity <-  update(m2.2, .~. + Parity)
+summary(m2.2Parity)
+m2.2Disease <- update(m2.2, .~. + Disease)
+summary(m2.2Disease)
+m2.2DIM <-  update(m2.2, .~. + DIM)
+summary(m2.2DIM)
+
+m2.3farm <- update(m2.3, .~. + Farm)
+summary(m2.3farm)
+m2.3Parity <-  update(m2.3, .~. + Parity)
+summary(m2.3Parity)
+m2.3Disease <- update(m2.3, .~. + Disease)
+summary(m2.3Disease)
+m2.3DIM <-  update(m2.3, .~. + DIM)
+summary(m2.3DIM)
+
+##PATTERN
+m3.1farm <- update(m3.1, .~. + Farm)
+summary(m3.1farm)
+z <- summary(m3.1farm)$coefficients/summary(m3.1farm)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+m3.1Parity <-  update(m3.1, .~. + Parity)
+summary(m3.1Parity)
+z <- summary(m3.1Parity)$coefficients/summary(m3.1Parity)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+m3.1Disease <- update(m3.1, .~. + Disease)
+summary(m3.1Disease)
+z <- summary(m3.1Disease)$coefficients/summary(m3.1Disease)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+m3.1DIM <-  update(m3.1, .~. + DIM)
+summary(m3.1DIM)
+z <- summary(m3.1DIM)$coefficients/summary(m3.1DIM)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+
+m3.1.0farm <- update(m3.1.0, .~. + Farm)
+summary(m3.1.0farm)
+z <- summary(m3.1.0farm)$coefficients/summary(m3.1.0farm)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+m3.1.0Parity <-  update(m3.1.0, .~. + Parity)
+summary(m3.1.0Parity)
+z <- summary(m3.1.0Parity)$coefficients/summary(m3.1.0Parity)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+m3.1.0Disease <- update(m3.1.0, .~. + Disease)
+summary(m3.1.0Disease)
+z <- summary(m3.1.0Disease)$coefficients/summary(m3.1.0Disease)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+m3.1.0DIM <-  update(m3.1.0, .~. + DIM)
+summary(m3.1.0DIM)
+z <- summary(m3.1.0DIM)$coefficients/summary(m3.1.0DIM)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+
+m3.2farm <- update(m3.2, .~. + Farm)
+summary(m3.2farm)
+z <- summary(m3.2farm)$coefficients/summary(m3.2farm)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+m3.2Parity <-  update(m3.2, .~. + Parity)
+summary(m3.2Parity)
+z <- summary(m3.2Parity)$coefficients/summary(m3.2Parity)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+m3.2Disease <- update(m3.2, .~. + Disease)
+summary(m3.2Disease)
+z <- summary(m3.2Disease)$coefficients/summary(m3.2Disease)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+m3.2DIM <-  update(m3.2, .~. + DIM)
+summary(m3.2DIM)
+z <- summary(m3.2DIM)$coefficients/summary(m3.2DIM)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+
+m3.3farm <- update(m3.3, .~. + Farm)
+summary(m3.3farm)
+z <- summary(m3.3farm)$coefficients/summary(m3.3farm)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+m3.3Parity <-  update(m3.3, .~. + Parity)
+summary(m3.3Parity)
+z <- summary(m3.3Parity)$coefficients/summary(m3.3Parity)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+m3.3Disease <- update(m3.3, .~. + Disease)
+summary(m3.3Disease)
+z <- summary(m3.3Disease)$coefficients/summary(m3.3Disease)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
+m3.3DIM <-  update(m3.3, .~. + DIM)
+summary(m3.3DIM)
+z <- summary(m3.3DIM)$coefficients/summary(m3.3DIM)$standard.errors
+z
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+p
